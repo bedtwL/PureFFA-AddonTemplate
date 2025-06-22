96e00b87a552d65cee3dd6de568842f117f7e565
bedtwL <blueaustin0507@gmail.com>
2025-06-22 15:29:46 +0800

    update api

--- src/main/java/me/example/Main.java
+++ src/main/java/me/example/Main.java
@@ -2,6 +2,8 @@ package me.example;
 
 import me.bedtwL.ffa.api.EffectAddon;
 
+import java.util.UUID;
+
 
 public class Main implements EffectAddon {
     @Override
@@ -12,6 +14,10 @@ public class Main implements EffectAddon {
     @Override
     public String getAuthor() {
         return "your name";
+    }
+    @Override
+    public UUID authorUUID() {
+        return UUID.fromString("your-mc-uuid");
     }
 
     @Override
